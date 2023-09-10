ac88e6baabc4ee4a10ef761630878d7cf2b290a6
lkjfrf <34626154+lkjfrf@users.noreply.github.com>
2023-09-11 04:39:29 +0900

    22

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -13,36 +13,40 @@ namespace DummyClient
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            try
+            while(true)
             {
-                // 휴대폰 설정
-                Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                try
+                {
+                    // 휴대폰 설정
+                    Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-                // 문지기한테 입장 문의
-                socket.Connect(endPoint);
-                // 블록킹 함수인 Connect가 통과되면 여길 지나감
-                Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
+                    // 문지기한테 입장 문의
+                    socket.Connect(endPoint);
+                    // 블록킹 함수인 Connect가 통과되면 여길 지나감
+                    Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
 
-                //보낸다
-                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World!");
-                int sendBytes = socket.Send(sendBuff);
+                    //보낸다
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World!");
+                    int sendBytes = socket.Send(sendBuff);
 
-                //받는다
-                byte[] recvBuff = new byte[1024];
-                int recvBytes = socket.Receive(recvBuff);
-                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-                Console.WriteLine($"[From Server] {recvData}");
+                    //받는다
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = socket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Server] {recvData}");
 
-                // 나간다
-                socket.Shutdown(SocketShutdown.Both);
-                socket.Close();
+                    // 나간다
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+
+                // 0.1초마나 보내고 끊고 보내고 끊고
+                Thread.Sleep(100);
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.ToString());
-            }
-
-
         }
     }
 }
--- ServerCore/Listener.cs
+++ ServerCore/Listener.cs
@@ -3,19 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    internal class Listener
+    class Listener
     {
         Socket _listenSocket;
+        Action<Socket> _onAcceptHandler;
 
-        public void Init(IPEndPoint endPoint)
+
+        public void Init(IPEndPoint endPoint, Action<Socket> onAcceptHandler)
         {
             _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _onAcceptHandler = onAcceptHandler;
+            _listenSocket.Bind(endPoint);
+            _listenSocket.Listen(10);
 
+            //SocketAsyncEventArgs 쓰는 이유는 AcceptAsync()함수를 활용하여 비동기 방식으로 처리할때
+            //바로 Accept가 되지않고 넘어갔을때 나중에 Accept성공되고 이벤트로 성공된 순간에 처리를 나중에 해주기 위함
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            //Completed 는 이벤트 헨들러 이므로 델리게이트를 추가해줌 
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+            //최초 등록
+            RegisterAccept(args);
+        }
+
+        void RegisterAccept(SocketAsyncEventArgs args)
+        {
+            // 만약 재등록 됐을때 다시 Accept 받을려면 깨끗한 상태에서 시작해야함
+            args.AcceptSocket = null;
+
+            // pending 을 뱉어줌
+            bool pending = _listenSocket.AcceptAsync(args);
+
+            // Accept가 바로 되었을때는 pending 이 false라서 바로 처리해줌
+            // Accept가 바로 안되었을때는 true 라서 OnAcceptComplete가 이벤트 발생될떄 처리됨
+            if (pending == false)
+                OnAcceptCompleted(null, args);
+        }
+
+        void OnAcceptCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            if (args.SocketError == SocketError.Success)
+            {
+                // 나중에 Accpet됐을때 등록해 주기위함
+                _onAcceptHandler.Invoke(args.AcceptSocket);
+            }
+            else
+                Console.WriteLine(args.SocketError.ToString());
+
+            //Accept 끝나서 처리가 완료되고 또 다음 Accept를 위해 또 등록
+            RegisterAccept(args);
+        }
+
+        public Socket Accept() 
+        {
+            //Accept, Recv, Send 같은 입출력 계열의 함수는 블록킹함수 이므로
+            //비동기로 변환해줘야 여러 사용자중 한명이 블록킹되어도 문제없도록 함
+            _listenSocket.AcceptAsync();    //sync = 동기, Async = 비동기
+            return _listenSocket.Accept();    
         }
     }
 }
